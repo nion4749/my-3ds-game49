a55ce850cca11270cb4f8313c237526c2bf09b6c
nion4749 <n.d.papanov@gmail.com>
2026-09-24 20:26:00 +0200

    Update main.c

--- sources/main.c
+++ sources/main.c
@@ -1,3 +1,20 @@
 //Uploaded by ROMSFOREVER.CO
 - Free ROMs & ISOs at ROMSFOREVER.CO
 - Please do not delete our credit when re-upload
+int main(int argc, char* argv[]) {
+    // Запуск вашей игры
+    gfxInitDefault();
+    
+    // Основной цикл программы 3DS
+    while (aptMainLoop()) {
+        hidScanInput();
+        u32 kDown = hidKeysDown();
+        if (kDown & KEY_START) break; // Выход при нажатии START
+        
+        gfxFlushBuffers();
+        gfxSwapBuffers();
+    }
+    
+    gfxExit();
+    return 0;
+}
